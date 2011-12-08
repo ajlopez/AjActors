0ff137f2979de097498b83cce00572575d6fa753
ajlopez <webmaster@ajlopez.com>
2011-12-08 17:26:27 -0300

    Refactor Actor to derive from BaseActor

--- Src/AjActors/Actor.cs
+++ Src/AjActors/Actor.cs
@@ -3,11 +3,9 @@ namespace AjActors
     using System;
     using System.Threading;
 
-    public class Actor<T>
+    public class Actor<T> : BaseActor
     {
         private T instance;
-        private BlockingQueue<ActionTask<T>> queue;
-        private bool running;
 
         public Actor(T instance)
         {
@@ -16,45 +14,7 @@ namespace AjActors
 
         public void Send(Action<T> action)
         {
-            if (!this.running)
-                this.Start();
-
-            this.queue.Enqueue(new ActionTask<T>(this.instance, action));
-        }
-
-        private void Start()
-        {
-            lock (this)
-            {
-                if (this.running)
-                    return;
-
-                this.queue = new BlockingQueue<ActionTask<T>>();
-
-                Thread thread = new Thread(new ThreadStart(this.Execute));
-                thread.IsBackground = true;
-                thread.Start();
-
-                this.running = true;
-            }
-        }
-
-        private void Execute()
-        {
-            while (true)
-            {
-                try
-                {
-                    ActionTask<T> task = this.queue.Dequeue();
-                    task.Execute();
-                }
-                catch (Exception ex)
-                {
-                    // TODO review output, maybe raise an event
-                    Console.Error.WriteLine(ex.Message);
-                    Console.Error.WriteLine(ex.StackTrace);
-                }
-            }
+            this.Send(new ActionTask<T>(this.instance, action));
         }
     }
 }
---
+++ Src/AjActors/BaseActor.cs
@@ -0,0 +1,57 @@
+﻿namespace AjActors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+
+    public class BaseActor
+    {
+        private BlockingQueue<ITask> queue;
+        private bool running;
+
+        public void Send(ITask task)
+        {
+            if (!this.running)
+                this.Start();
+
+            this.queue.Enqueue(task);
+        }
+
+        private void Start()
+        {
+            lock (this)
+            {
+                if (this.running)
+                    return;
+
+                this.queue = new BlockingQueue<ITask>();
+
+                Thread thread = new Thread(new ThreadStart(this.Run));
+                thread.IsBackground = true;
+                thread.Start();
+
+                this.running = true;
+            }
+        }
+
+        private void Run()
+        {
+            while (true)
+            {
+                try
+                {
+                    ITask task = this.queue.Dequeue();
+                    task.Execute();
+                }
+                catch (Exception ex)
+                {
+                    // TODO review output, maybe raise an event
+                    Console.Error.WriteLine(ex.Message);
+                    Console.Error.WriteLine(ex.StackTrace);
+                }
+            }
+        }
+    }
+}
