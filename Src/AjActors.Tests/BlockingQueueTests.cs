7f4e6883d0c520a47a8db83a30a036f4db1199a5
ajlopez <webmaster@ajlopez.com>
2011-12-08 16:05:00 -0300

    BlockingQueue, with tests

---
+++ Src/AjActors.Tests/BlockingQueueTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+namespace AjActors.Tests
+{
+    [TestClass]
+    public class BlockingQueueTests
+    {
+        [TestMethod]
+        public void CreateAndUseBlockingQueue()
+        {
+            BlockingQueue<int> queue = new BlockingQueue<int>(1);
+
+            Thread thread = new Thread(new ThreadStart(delegate() { queue.Enqueue(1); }));
+            thread.Start();
+
+            int element = queue.Dequeue();
+            Assert.AreEqual(1, element);
+        }
+
+        [TestMethod]
+        public void CreateAndUseBlockingQueueTenTimes()
+        {
+            BlockingQueue<int> queue = new BlockingQueue<int>(5);
+
+            Thread thread = new Thread(new ThreadStart(delegate() { for (int k=1; k<=10; k++) queue.Enqueue(k); }));
+            thread.Start();
+
+            for (int j = 1; j <= 10; j++)
+            {
+                int element = queue.Dequeue();
+                Assert.AreEqual(element, j);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void RaiseIfZeroInConstructor()
+        {
+            new BlockingQueue<string>(0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void RaiseIfNegativeNumberInConstructor()
+        {
+            new BlockingQueue<string>(-1);
+        }
+    }
+}
+
---
+++ Src/AjActors/BlockingQueue.cs
@@ -0,0 +1,52 @@
+﻿namespace AjActors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+
+    public class BlockingQueue<T>
+    {
+        private Queue<T> queue = new Queue<T>();
+        private int maxsize;
+
+        public BlockingQueue()
+            : this(100)
+        {
+        }
+
+        public BlockingQueue(int maxsize)
+        {
+            if (maxsize <= 0)
+                throw new InvalidOperationException("BlockingQueue needs a positive maxsize");
+
+            this.maxsize = maxsize;
+        }
+
+        public void Enqueue(T element)
+        {
+            lock (this)
+            {
+                while (this.queue.Count >= this.maxsize)
+                    Monitor.Wait(this);
+
+                this.queue.Enqueue(element);
+                Monitor.PulseAll(this);
+            }
+        }
+
+        public T Dequeue()
+        {
+            lock (this)
+            {
+                while (this.queue.Count == 0)
+                    Monitor.Wait(this);
+
+                T element = this.queue.Dequeue();
+                Monitor.PulseAll(this);
+                return element;
+            }
+        }
+    }
+}
